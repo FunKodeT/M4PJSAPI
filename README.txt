e243659b3af88259435d18887982606bffee0fa7
FunKodeT <cooteybug@gmail.com>
2024-04-10 17:31:12 -0600

    2.3; Major Update: Overall webpage design and fundamental concept instituted in this version; Updated: script.js, styles.css, README.txt

--- README.txt
+++ README.txt
@@ -35,12 +35,26 @@
 		- Pushed local repo updates to remote repo 'origin'
 		- Updated:
 			~ README.txt					[dir/]
+	**VERSION 0.2**
+		- Fixed index.html from '1' merged into '0'
+		- Updated:
+			~ README.txt					[dir/]
 				
 **BRANCH 1**
 	**VERSION 1.0**
 		- Updated:
 			~ index.html					[dir/]
 				^0.0	=	1.0^
+			~ README.txt					[dir/]
+	**VERSION 1.1**
+		- Modified index.html to be the same as assignment provided code for troubleshooting purposes
+		- Updated:
+			~ index.html					[dir/]
+			~ README.txt					[dir/]
+	**VERSION 1.2**
+		- Comparisons between my code and later provided code shows a typo in the URL; Corrections were made, full functionality is now availabe
+		- Updated:
+			~ index.html					[dir/]
 			~ README.txt					[dir/]
 
 **BRANCH 2**
@@ -77,3 +91,12 @@
 			~ README.txt					[dir/]
 		- Removed:
 			~ business.json					[dir/]
+	**VERSION 2.3**
+		- 'script.js' has been updated with code to create dynamic card elements in the index.html for displaying information obtained from the 'business.json' API
+		- 'styles.css' has been updated to provide a desirable set of cards that display the information in a cute, easily readable fashion
+		- Updated:
+			~ script.js						[dir/]
+				^2.1	=	2.3^
+			~ styles.css					[dir/]
+				^2.1	=	2.3^
+			~ README.txt					[dir/]
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// #######################################################################################################
 const URL = "https://iskarr.github.io/austindonovan.github.io/api/business.json";
     /*
         This sets a variable named URL to pull API data from
@@ -6,60 +7,201 @@ let cards = document.querySelector("div.cards");
     /*
         This sets a variable equal to the <div class="cards"> tag
     */
-let para = document.createElement("p");
-    /*
-        This sets a variable that creates a <p></p> tag element in the HTML
-    */
+// =======================================================================================================
 
-// API Fetch
 
+
+//                                              API FETCH URL
+// #######################################################################################################
 fetch(URL)
 .then((response) => response.json())
 .then((jsObject) => {
 let business = jsObject.business;
-
-
-for (let i = 0; i < business.length; i++) {
+console.log(business);
+for(let i = 0; i < business.length; i++){
 // console.log(business[i].name);
-
+// -------------------------------------------------------------------------------------------------------
 /*
 You must include your javascript below to display the following information:
-    1. Business names
-    2. Business image
+    // 1. Business names
+    // 2. Business image
     3. Business locations
     4. Business descriptions
 Here is an example for the name to start you off:
 */
+// =======================================================================================================
 
+
+
+//                               ESTABLISHING THE 'CARD' ELEMENT AND RULES
+// #######################################################################################################
 let card = document.createElement("section");
     /*
         Creates card and places business name in h2(MC: I believe this is supposed to say 'section') element
     */
 card.className = "card";
     /*
-        Adds a classname to the section element above
+        Adds a class attribute to the div tag
     */
+// =======================================================================================================
+
+
+
+//                                  ESTABLISHING A 'CONTAINER' ELEMENT
+// #######################################################################################################
+let cardContainer = document.createElement("div");
+    /*
+        Creates a 'container' for the front and back elements of the cards
+    */
+cardContainer.className = "cardContainer";
+    /*
+        Adds a class attribute to the div tag
+    */
+// =======================================================================================================
+
+
+
+//                              ESTABLISHING A 'FRONT CARD' ELEMENT AND RULES
+// #######################################################################################################
+let cardFront = document.createElement("div");
+    /*
+        Creates a 'front' side of the card element
+    */
+cardFront.className = "cardFront";
+    /*
+        Adds a class attribute to the div tag
+    */
+// =======================================================================================================
+
+
+
+//                              ESTABLISHING A 'BACK CARD' ELEMENT AND RULES
+// #######################################################################################################
+let cardBack = document.createElement("div");
+    /*
+        Creates a 'back' side of the card element
+    */
+cardBack.className = "cardBack";
+    /*
+        Establishes a class element for the div html
+    */
+// =======================================================================================================
+
+
+
+//                   ESTABLISHING A 'BUSINESS NAME' SECTION FOR THE FRONT FACE OF A CARD
+// #######################################################################################################
 let h2 = document.createElement("h2");
     /*
         This sets a variable that creates a <h2></h2> tag element in the HTML
     */
 h2.textContent = business[i].name;
     /*
-        This tells the h2 variable that its text content will be equal to the associated businesses name
+        This tells the h2 variable that its text content will be equal to the associated business iterator's name
     */
-card.appendChild(h2);
+cardFront.appendChild(h2);
     /*
-        This tells card variable to have a child equal to the h2 variable
+        This tells the cardFront variable to have a child equal to the h2 variable
     */
+// =======================================================================================================
+
+
+
+//                                      Include a business location
+// #######################################################################################################
+//                  ESTABLISHING A 'BUSINESS LOCATION' SECTION FOR THE FRONT FACE OF A CARD
+// -------------------------------------------------------------------------------------------------------
+let ul = document.createElement("ul");
+    /*
+        This sets a variable that creates a <ul></ul> tag element in the HTML
+    */
+let li = document.createElement("li");
+    /*
+        This sets a variable that creates a <li></li> tag element in the HTML
+    */
+li.textContent = business[i].address;
+    /*
+        This tells the li variable that its text content will be equal to the associated business iterator's address
+    */
+ul.appendChild(li);
+    /*
+        This tells the ul variable to have a child that is equal to the li variable
+    */
+cardFront.appendChild(ul);
+    /*
+        This tells the cardFront variable to have a child that is equal to the ul variable
+    */
+// =======================================================================================================
+
+
+
+//          Create the image location, you can look up how to add image using javascript
+// #######################################################################################################
+//          ESTABLISHING A 'BUSINESS BACKGROUND IMAGES' SECTION FOR THE FRONT FACE OF A CARD
+// -------------------------------------------------------------------------------------------------------
+let img = document.createElement("img");
+    /*
+        This sets a variable that creates a <img> tag element in the HTML
+    */
+img.srcset = business[i].imageurl;
+    /*
+        This tells the img variable that its src will be equal to the associated business iterator's image
+    */
+cardFront.appendChild(img);
+    /*
+        This tells the cardFront variable to have a child that is equal to the img variable
+    */
+// =======================================================================================================
+
+
+
+//                                  Include a business Description
+// #######################################################################################################
+//                  ESTABLISHING A 'BUSINESS DESCRIPTION' SECTION FOR THE BACK FACE OF A CARD
+let para = document.createElement("p");
+    /*
+        This sets a variable that creates a <p></p> tag element in the HTML
+    */
+para.textContent = business[i].description;
+    /*
+        This tells the para variable that its text content will be equal to the association business iterator's description
+    */
+cardBack.appendChild(para);
+    /*
+        This tells the cardBack variable to have a child that is equal to the para variable
+    */
+// =======================================================================================================
+
+
+
+//                          APPLYING CHILD ELEMENTS TO THE CARD
+// #######################################################################################################
+cardContainer.appendChild(cardFront);
+    /*
+        This tell the card variable to have a child that is equal to the cardFront variable
+    */
+cardContainer.appendChild(cardBack);
+    /*
+        This tells the card variable to have a child that is equal to the cardBack variable
+    */
+card.appendChild(cardContainer);
+// =======================================================================================================
+
+
+
+//                      APPLYING CARD ELEMENTS TO CARDS SECTION OF THE HTML DOCUMENT
+// #######################################################################################################
 document.querySelector("div.cards").appendChild(card);
     /*
-        This tells the <div class="cards"> tag to have a child equal to the card variable
+        This tells the HTML class "cards" associated with a div tag to have a child that is equal to the card variable
     */
+// =======================================================================================================
 
-// Create the image location, you can look up how to add image using javascript
 
-// Include a business location
 
-// Include a business Description
 }
 });
+
+
+
+// =======================================================================================================
